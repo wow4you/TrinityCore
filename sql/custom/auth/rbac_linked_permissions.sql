f8fdc9972c6d3a075d176508658e11a09f8b77e9
Dragonfly <kid_10@gmx.at>
2014-08-26 22:44:15 +0200

    Some custom faction changes (Allow almost everything between factions!)

---
+++ sql/custom/auth/rbac_linked_permissions.sql
@@ -0,0 +1,17 @@
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '24';
+INSERT INTO rbac_linked_permissions ('195', '24'); -- Player: Two side faction characters on the same account
+
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '25';
+INSERT INTO rbac_linked_permissions ('195', '25'); -- Player: Allow say chat between factions
+
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '26';
+INSERT INTO rbac_linked_permissions ('195', '26'); -- Player: Allow channel chat between factions
+
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '27';
+INSERT INTO rbac_linked_permissions ('195', '27'); -- Player: Allow two side mail interaction
+
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '28';
+INSERT INTO rbac_linked_permissions ('195', '28'); -- Player: See two side who list
+
+DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '29';
+INSERT INTO rbac_linked_permissions ('195', '29'); -- Player: Add friends of other faction
