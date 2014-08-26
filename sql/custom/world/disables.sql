2338c749bafa00eb54944fa669de54813b966fe4
Dragonfly <kid_10@gmx.at>
2014-08-26 22:50:59 +0200

    Disable mmaps for ToC

---
+++ sql/custom/world/disables.sql
@@ -0,0 +1,5 @@
+DELETE FROM disables WHERE sourceType = '7' AND entry = '649';
+INSERT INTO disables VALUES (7, 649, 0, '', '', 'Mmaps - Trial of the Crusader');
+
+DELETE FROM disables WHERE sourceType = '7' AND entry = '650';
+INSERT INTO disables VALUES (7, 650, 0, '', '', 'Mmaps - Trial of the Champion');
