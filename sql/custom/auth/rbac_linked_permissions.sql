d7d7788dba451f0ccf74cd5a2dffb7d8a81f1b98
Huricane <Huricane@users.noreply.github.com>
2014-09-06 21:11:32 +0200

    Fix mistake in previous commit b454c3a

--- sql/custom/auth/rbac_linked_permissions.sql
+++ sql/custom/auth/rbac_linked_permissions.sql
@@ -1,6 +1,6 @@
 DELETE FROM `rbac_linked_permissions` WHERE `id` IN(192, 193, 194, 195) AND `linkedId` IN(24, 25, 26, 27, 28, 29);
 INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES
-(195, 24); -- Player: Two side faction characters on the same account
+(195, 24), -- Player: Two side faction characters on the same account
 (195, 25), -- Player: Allow say chat between factions
 (195, 26), -- Player: Allow channel chat between factions
 (195, 27), -- Player: Allow two side mail interaction
