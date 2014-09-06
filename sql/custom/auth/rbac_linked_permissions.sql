b7296f5070e415c57c41c9ef4658b05a71e07bd7
Huricane <Huricane@users.noreply.github.com>
2014-09-06 21:09:08 +0200

    Fix and optimize rbac_linked_permissions.sql

--- sql/custom/auth/rbac_linked_permissions.sql
+++ sql/custom/auth/rbac_linked_permissions.sql
@@ -1,17 +1,8 @@
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '24';
-INSERT INTO rbac_linked_permissions ('195', '24'); -- Player: Two side faction characters on the same account
-
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '25';
-INSERT INTO rbac_linked_permissions ('195', '25'); -- Player: Allow say chat between factions
-
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '26';
-INSERT INTO rbac_linked_permissions ('195', '26'); -- Player: Allow channel chat between factions
-
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '27';
-INSERT INTO rbac_linked_permissions ('195', '27'); -- Player: Allow two side mail interaction
-
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '28';
-INSERT INTO rbac_linked_permissions ('195', '28'); -- Player: See two side who list
-
-DELETE FROM rbac_linked_permissions WHERE id = '195' AND linkedId = '29';
-INSERT INTO rbac_linked_permissions ('195', '29'); -- Player: Add friends of other faction
+DELETE FROM `rbac_linked_permissions` WHERE `id` IN(192, 193, 194, 195) AND `linkedId` IN(24, 25, 26, 27, 28, 29);
+INSERT INTO `rbac_linked_permissions`(`id`, `linkedId`) VALUES
+(195, 24); -- Player: Two side faction characters on the same account
+(195, 25), -- Player: Allow say chat between factions
+(195, 26), -- Player: Allow channel chat between factions
+(195, 27), -- Player: Allow two side mail interaction
+(195, 28), -- Player: See two side who list
+(195, 29); -- Player: Add friends of other faction
